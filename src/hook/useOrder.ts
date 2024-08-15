671e9915226dbdf785b2d16acb8da050a006894e
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2024-08-14 22:44:53 -0600

    Agregnado hook para ordenes / added order hook

---
+++ src/hook/useOrder.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import type { MenuItem, orderItem } from "../types";
+
+export default function useOrder() {
+  const [order, setOrder] = useState<orderItem[]>([]);
+
+  const addItem = (item: MenuItem) => {
+    // console.log("item",item);
+
+    const itemExists = order.find((orderItem) => orderItem.id === item.id);
+
+    if (itemExists) {
+      const updatedOrder = order.map((orderItem) =>
+        orderItem.id === item.id
+          ? { ...orderItem, quantity: orderItem.quantity + 1 }
+          : orderItem
+      );
+
+      setOrder(updatedOrder);
+    } else {
+      const newItem: orderItem = { ...item, quantity: 1 };
+      setOrder([...order, newItem]);
+    }
+
+    // console.log("order", order);
+  };
+
+  return {
+    order,
+    addItem,
+  };
+}
